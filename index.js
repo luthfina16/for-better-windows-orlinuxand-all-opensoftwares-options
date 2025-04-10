28680a3db9b2f32c0fde0451dca01087c31f1a9d
Luthfi Nur Ariri <160535600+luthfina16@users.noreply.github.com>
2025-04-10 11:16:42 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,108 +1,101 @@
-'use strict';
+const puppeteer = require('puppeteer');
+var moment = require('moment-timezone');
 
-var parseUrl = require('url').parse;
 
-var DEFAULT_PORTS = {
-  ftp: 21,
-  gopher: 70,
-  http: 80,
-  https: 443,
-  ws: 80,
-  wss: 443,
-};
+// (async () => {
+//     const browser = await puppeteer.launch({
+//         executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
+//         userDataDir: '%LOCALAPPDATA%\Google\Chrome\User Data',
+//         headless: false
+//     });
+//     const page = await browser.newPage();
+//     await page.goto('https://shopee.co.id/Apple-iPhone-SE-2nd-Gen-128GB-Black-i.241308147.7953405952');
+//     await page.screenshot({ path: 'example.png' });
+//    // await new Promise(resolve => setTimeout(resolve, 1500000))
+//     await browser.close();
+// })();
+const main = async () => {
+    const browser = await puppeteer.launch({
+        executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
+        userDataDir: './user_data',
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+          //  '--disable-accelerated-2d-canvas',
+            //'--no-first-run',
+          //  '--no-zygote',
+          //  '--single-process', // <- this one doesn't works in Windows
+            '--disable-gpu'
+          ],
+        headless: false
+    });
+    const page = await browser.newPage();
+    const waitForLoad = new Promise(resolve => page.on('load', () => resolve()));
+    while (true) {
+        let url = "https://shopee.co.id/Emas-UBS-0.5-Gram-Logam-Mulia-Garansi-Uang-Kembali-Include-Sertifikat-i.13385605.440060031";
+        let tes = "https://shopee.co.id/BAREL-RJ45-DOUBLE-CABANG-(BARREL-UTP-DOUBLE)-i.4622483.46014082";
+        var time = moment().tz("Asia/Jakarta").format('h:mm:ss');
+        await page.goto(url);
+        //await page.setCacheEnabled(false);
 
-var stringEndsWith = String.prototype.endsWith || function(s) {
-  return s.length <= this.length &&
-    this.indexOf(s, this.length - s.length) !== -1;
-};
+        //Menunggu semua yang ada
+        waitForLoad;
+        // await page.waitForNavigation({ waitUntil: 'networkidle2' });
+        //mengecek yang perlu
+        await page.waitForSelector('button.btn.btn-tinted.btn--l.btn-tinted--disabled.YtgjXY._3a6p6c', { timeout: 1000 }).catch(() => console.log(`time: ${time}. tombol tidak ada!!`));
+        await page.waitForSelector('button.btn.btn-tinted.btn--l.YtgjXY._3a6p6c', { timeout: 5000 }).catch(() => console.log(`time: ${time}. tombol 2 tidak ada!!`));
+        const is_disabled = await page.evaluate(() => document.querySelector('button.btn.btn-tinted.btn--l.btn-tinted--disabled.YtgjXY._3a6p6c') !== null);
+        const autoClick = await page.evaluate(() => document.querySelector('button.btn.btn-tinted.btn--l.YtgjXY._3a6p6c').click()).catch(() => console.log(`time: ${time}. tombol 2 tidak ada!!`));
+        const delay = await new Promise(resolve => setTimeout(resolve, 100));
 
-/**
- * @param {string|object} url - The URL, or the result from url.parse.
- * @return {string} The URL of the proxy that should handle the request to the
- *  given URL. If no proxy is set, this will be an empty string.
- */
-function getProxyForUrl(url) {
-  var parsedUrl = typeof url === 'string' ? parseUrl(url) : url || {};
-  var proto = parsedUrl.protocol;
-  var hostname = parsedUrl.host;
-  var port = parsedUrl.port;
-  if (typeof hostname !== 'string' || !hostname || typeof proto !== 'string') {
-    return '';  // Don't proxy URLs without a valid scheme or host.
-  }
 
-  proto = proto.split(':', 1)[0];
-  // Stripping ports in this way instead of using parsedUrl.hostname to make
-  // sure that the brackets around IPv6 addresses are kept.
-  hostname = hostname.replace(/:\d*$/, '');
-  port = parseInt(port) || DEFAULT_PORTS[proto] || 0;
-  if (!shouldProxy(hostname, port)) {
-    return '';  // Don't proxy URLs that match NO_PROXY.
-  }
 
-  var proxy =
-    getEnv('npm_config_' + proto + '_proxy') ||
-    getEnv(proto + '_proxy') ||
-    getEnv('npm_config_proxy') ||
-    getEnv('all_proxy');
-  if (proxy && proxy.indexOf('://') === -1) {
-    // Missing scheme in proxy, default to the requested URL's scheme.
-    proxy = proto + '://' + proxy;
-  }
-  return proxy;
+        // page.on('response', response => {
+        //     console.log(time + '.' + response.status())
+        //     if (response.url().endsWith("api/v2/cart/add_to_cart")) {
+        //         console.log(`time: ${time}. add to cart response code:  ${response}`);
+        //     }})
+            // do something here
+
+            if (is_disabled) {
+                console.log(`time: ${time}.  masih disable-_`)
+
+            }
+            else {
+                console.log(`time: ${time}. sudah unlock gasss`)
+                
+                    delay;
+                    autoClick;
+                
+            }
+        
+        //await new Promise(resolve => setTimeout(resolve, 00))
+        //await browser.close();
+        await new Promise(resolve => setTimeout(resolve, 100));
+
+    }
+    // await page.evaluate(() => {
+
+    //     console.log(document.querySelector('div.xns9gb').values());
+    // })
+    // Type "JavaScript" into the search bar
+
+    //   // Click on the "Google Search" button and wait for the page to load
+    //   const waitForLoad = new Promise(resolve => page.on('load', () => resolve()));
+    //   await page.evaluate(() => {
+    //     document.querySelector('input[value="Google Search"]').click();
+    //   });
+    //   await waitForLoad;
+
+    //   // Get all the search result URLs
+    //   const links = await page.evaluate(function getUrls() {
+    //     return Array.from(document.querySelectorAll('a cite').values()).
+    //       map(el => el.innerHTML);
+    //   });
+    //   console.log(links);
+
+
+
 }
-
-/**
- * Determines whether a given URL should be proxied.
- *
- * @param {string} hostname - The host name of the URL.
- * @param {number} port - The effective port of the URL.
- * @returns {boolean} Whether the given URL should be proxied.
- * @private
- */
-function shouldProxy(hostname, port) {
-  var NO_PROXY =
-    (getEnv('npm_config_no_proxy') || getEnv('no_proxy')).toLowerCase();
-  if (!NO_PROXY) {
-    return true;  // Always proxy if NO_PROXY is not set.
-  }
-  if (NO_PROXY === '*') {
-    return false;  // Never proxy if wildcard is set.
-  }
-
-  return NO_PROXY.split(/[,\s]/).every(function(proxy) {
-    if (!proxy) {
-      return true;  // Skip zero-length hosts.
-    }
-    var parsedProxy = proxy.match(/^(.+):(\d+)$/);
-    var parsedProxyHostname = parsedProxy ? parsedProxy[1] : proxy;
-    var parsedProxyPort = parsedProxy ? parseInt(parsedProxy[2]) : 0;
-    if (parsedProxyPort && parsedProxyPort !== port) {
-      return true;  // Skip if ports don't match.
-    }
-
-    if (!/^[.*]/.test(parsedProxyHostname)) {
-      // No wildcards, so stop proxying if there is an exact match.
-      return hostname !== parsedProxyHostname;
-    }
-
-    if (parsedProxyHostname.charAt(0) === '*') {
-      // Remove leading wildcard.
-      parsedProxyHostname = parsedProxyHostname.slice(1);
-    }
-    // Stop proxying if the hostname ends with the no_proxy host.
-    return !stringEndsWith.call(hostname, parsedProxyHostname);
-  });
-}
-
-/**
- * Get the value for an environment variable.
- *
- * @param {string} key - The name of the environment variable.
- * @return {string} The value of the environment variable.
- * @private
- */
-function getEnv(key) {
-  return process.env[key.toLowerCase()] || process.env[key.toUpperCase()] || '';
-}
-
-exports.getProxyForUrl = getProxyForUrl;
+main().catch(console.error)
